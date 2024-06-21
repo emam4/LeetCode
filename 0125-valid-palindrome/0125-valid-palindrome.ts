c3b25d8ed934595c867b5276100d78f4b301f81a
Nour-Eldin Emam <119869254+emam4@users.noreply.github.com>
2024-06-21 12:26:48 +0300

    Time: 56 ms (94.28%), Space: 52.4 MB (81.24%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,13 +1,8 @@
 function isPalindrome(s: string): boolean {
     const text = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase().trim();
     
-    // if(s.length === 1 || s.length === 2) return true;
-    // if(/^\s*$/.test(s)) return true;
-    // if(text.length %2 === 0) return false;
-    // if(text.length === 1) return true;
-    
-    let front: number = 0;              // 0  1  2  3
-    let back: number = text.length -1;  // 5  4  3  2
+    let front: number = 0;
+    let back: number = text.length -1;
     while(front < back) {
         if(text[front] !== text[back]) {
             return false;
