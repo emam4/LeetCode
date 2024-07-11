876ed9c84565e702425601b79f2d157995477641
Nour-Eldin Emam <119869254+emam4@users.noreply.github.com>
2024-07-11 22:38:41 +0300

    Time: 117 ms (28.69%), Space: 55.9 MB (36.67%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -15,10 +15,15 @@ var addTwoNumbers = function(l1, l2) {
     let carry = 0;
     while(l1 || l2 || carry) {
         let sum = (l1?.val || 0) + (l2?.val || 0) + carry;
-        const val = sum % 10;
-        carry = Math.floor(sum / 10);
+        let val;
+        if(sum >= 10 ) {
+            carry = 1;
+            sum-=10
+        } else { carry = 0 }
+        // const val = sum % 10;
+        // carry = Math.floor(sum / 10);
         
-        tail.next = new ListNode(val);
+        tail.next = new ListNode(sum);
         tail = tail.next;
         
         l1 = l1?.next;
