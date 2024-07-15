fb35dfcc960bceb70cf3514cf7b64ece9d433dc8
Nour-Eldin Emam <119869254+emam4@users.noreply.github.com>
2024-07-15 23:26:48 +0300

    Time: 66 ms (90.70%), Space: 56.7 MB (77.46%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function isSameTree(s: TreeNode | null, t: TreeNode | null): boolean {
+    if (s === null && t === null) return true;
+    if (s === null || t === null) return false;
+    return s.val === t.val && isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+}
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+    if (root === null) return false;
+    if (isSameTree(root, subRoot)) return true;
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+}
