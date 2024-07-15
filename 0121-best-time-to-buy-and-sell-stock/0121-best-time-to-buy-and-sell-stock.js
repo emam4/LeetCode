cf6180fe8d345ecc8e711898dcb55160e583d50b
Nour-Eldin Emam <119869254+emam4@users.noreply.github.com>
2024-07-15 09:50:40 +0300

    Time: 84 ms (18.48%), Space: 59 MB (55.31%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -6,13 +6,13 @@ var maxProfit = function(prices) {
     let [l, r] = [0, 1];
     let maxProfit = 0;
     while(r < prices.length) {
-        if(prices[r] > prices[l]) { 
-            const profit = prices[r] - prices[l];
+        if(prices[r] > prices[l]) {
+            const profit = prices[r] - prices[l]
             if(profit > maxProfit) maxProfit = profit;
         } else {
             l = r
         }
-      
+        
         r++;
     }
     
