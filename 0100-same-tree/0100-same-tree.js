5b91c4deb52bff09de02ebd9a1e1fc19fe7bf748
Nour-Eldin Emam <119869254+emam4@users.noreply.github.com>
2024-07-15 23:56:07 +0300

    Time: 54 ms (50.97%), Space: 49.2 MB (47.33%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -13,7 +13,9 @@
  */
 var isSameTree = function(p, q) {
     if(!p && !q) return true;
-    if(!p || !q) return false;
+    if((!p && q) || (p && !q)) return false;
+    if(p.val !== q.val) return false;
 
-    return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
